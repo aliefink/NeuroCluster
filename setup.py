c55c78ee2f827641eb09936678a0dcdaab523b3e
aliefink <alexandra.fink@icahn.mssm.edu>
2024-09-02 16:36:13 -0400

    update template and yml file

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='NeuroCluster',
-    version='0.1.0',
+    version='0.2.0',
     description='Non-parametric cluster-based permutation testing with time-frequency resolution',
     long_description=open('README.md').read(),
     url=github_url,
